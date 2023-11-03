31da5405b46f1df14ad68c7895b6206f224f2b61
Sutirtha Chakraborty <sutirtha.nitm@gmail.com>
2023-11-03 18:12:43 +0000

    updated one file

--- tests/test_align_face.py
+++ tests/test_align_face.py
@@ -6,7 +6,7 @@ from mediapipe_face_align.mediapipe_face_align import align_face
 def test_align_face():
     # Create a fake image and fake landmarks
     fake_image = np.zeros((100, 100, 3), dtype=np.uint8)
-    fake_landmarks = [(10, 10), (90, 90)]
+    fake_landmarks = [(x, y) for x in range(360) for y in range(360)]
 
     # Call the function with fake data
     aligned_image, M = align_face(fake_image, fake_landmarks)
