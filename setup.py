6fce965484809c49afeffb470af3133f71b19859
Sutirtha Chakraborty <sutirtha.nitm@gmail.com>
2023-11-03 16:51:15 +0000

    Updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="myfacealignment",
+    name="mediapipe_face_align",
     version="0.1",
     description="A package for face alignment",
-    author="Your Name",
+    author="Sutirtha Chakraborty",
     author_email="sutirtha@example.com",
     url="https://github.com/SutirthaChakraborty/Mediapipe_Face_align",
     packages=find_packages(),
