8a1d11d99712b5af452680d7cd627841b25a0c54
Sutirtha Chakraborty <sutirtha.nitm@gmail.com>
2023-11-03 18:19:08 +0000

    updated

--- tests/test_align_face.py
+++ tests/test_align_face.py
@@ -6,7 +6,10 @@ from mediapipe_face_align.mediapipe_face_align import align_face
 def test_align_face():
     # Create a fake image and fake landmarks
     fake_image = np.zeros((100, 100, 3), dtype=np.uint8)
-    fake_landmarks = [(x, y) for x in range(360) for y in range(360)]
+    # Place fake landmarks at approximate positions where eyes might be located
+    fake_landmarks = [(30, 30), (70, 30)] * 360  # Duplicating to fill in other indices, but keeping eyes constant.
+    fake_landmarks[130] = (30, 30)  # Left eye
+    fake_landmarks[359] = (70, 30)  # Right eye
 
     # Call the function with fake data
     aligned_image, M = align_face(fake_image, fake_landmarks)
--- tests/test_transform_landmarks.py
+++ tests/test_transform_landmarks.py
@@ -2,14 +2,16 @@
 import numpy as np
 from mediapipe_face_align.mediapipe_face_align import transform_landmarks
 
+import numpy.testing as npt
 
 def test_transform_landmarks():
     # Create a fake landmarks and a transformation matrix
-    fake_landmarks = [(10, 10), (90, 90)]
-    M = np.array([[1, 0, 10], [0, 1, 20]])
+    fake_landmarks = np.array([(10, 10), (90, 90)])
+    M = np.array([[1, 0, 10], [0, 1, 20], [0, 0, 1]])
 
     # Call the function with fake data
     transformed = transform_landmarks(fake_landmarks, M)
 
     # Check that the landmarks are correctly transformed
-    assert transformed == [(20, 30), (100, 110)]
+    expected_transformed = np.array([(20, 30), (100, 110)])
+    npt.assert_array_equal(transformed, expected_transformed)
